f33214d7954b9b32fc6933cf88353265ad15a6e4
Massimiliano Calamelli <mcalamelli@gmail.com>
2017-06-22 20:20:10 +0200

    Un nuovo tipo di creatura

---
+++ pylife/a2.py
@@ -0,0 +1,9 @@
+# pylint: disable=locally-disabled,missing-docstring,invalid-name,line-too-long
+# -*- coding: utf-8 -*-
+
+#from random import randrange
+from a0 import a0
+
+
+class a2(a0):
+    pass
