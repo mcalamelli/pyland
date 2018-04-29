99585bfc446f65f600cefdfe772c2cc6f5a09834
Massimiliano Calamelli <mcalamelli@gmail.com>
2018-04-29 22:21:58 +0200

    It basically works

--- training_set_gen.py
+++ training_set_gen.py
@@ -1,1 +1,15 @@
 # pylint: disable=locally-disabled,missing-docstring,invalid-name,line-too-long
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dict', '-d',
+                    type=argparse.FileType('r'),
+                    metavar='<dict>',
+                    help='Specifica il file dizionario.')
+args = parser.parse_args()
+
+#print('Analisi del file: %s\n' % args.dict.name)
+with open("training_set.txt", "w") as output_file:
+    for line in open(args.dict.name):
+        print("Inverti {}".format(line), file=output_file, end='')
+        print("Inverti la parola {}".format(line), file=output_file, end='')
